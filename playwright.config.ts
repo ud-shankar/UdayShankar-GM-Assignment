c6058de734c500b435d1a8c67a17168d6f52f874
ud-shankar <shankaruday1995@gmail.com>
2023-09-12 18:47:21 +0530

    Added .env file and seperated out API end points

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,4 +1,5 @@
 import { defineConfig, devices } from '@playwright/test';
+require('dotenv').config();
 
 export default defineConfig({
   testDir: './tests',
@@ -10,7 +11,6 @@ export default defineConfig({
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'html',
   use: {
-    baseURL: 'https://demoqa.com',
     headless:true,
     screenshot: 'only-on-failure'
   },
--- tests/e2e-API/accounts.spec.ts
+++ tests/e2e-API/accounts.spec.ts
@@ -1,12 +1,14 @@
 import { test } from "@playwright/test";
 import { APILibs } from "../../common-lib/api_lib";
 import { APITestData } from "../../test-data/api_test_data";
+import { AccountsURI } from "../../api-end-points/account";
 
 const apiLibs = new APILibs();
+const account_end_point = new AccountsURI();
 const apiData = new APITestData();
 
 test('Creation of a new user account', async ({request}) =>{
-    const response = await request.post(apiData.createUser, {data:{
+    const response = await request.post(account_end_point.createUser, {data:{
             "userName": apiData.userName,
             "password": apiData.validPassWord
     }});
@@ -14,7 +16,7 @@ test('Creation of a new user account', async ({request}) =>{
 });
 
 test('Verify creation of a new user account with invalid password', async ({request}) => {
-    const response = await request.post(apiData.createUser, {data:{
+    const response = await request.post(account_end_point.createUser, {data:{
         "userName": apiData.userName,
         "password": apiData.invalidPassword
 }});
